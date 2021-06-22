fbcb4430ed8662816c8fadbc8d8701c8364978f5
FireM <fire.mars.j@163.com>
2021-06-22 15:20:32 +0800

    transformations

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,17 @@
 <template>
   <div>
-    <h1>v-for-v-if-precedence-change</h1>
-    <v-for-v-if-precedence-change></v-for-v-if-precedence-change>
-    <h1>v-bind-order-sensitive</h1>
-    <v-bind-order-sensitive></v-bind-order-sensitive>
-    <h1>v-else-if-key</h1>
-    <v-else-if-key></v-else-if-key>
-    <h1>transition-group-root</h1>
-    <transition-group-root></transition-group-root>
-    <h1>v-for-template-key</h1>
-    <v-for-template-key></v-for-template-key>
+    <div>HelloWorld!</div>
+    <transformations></transformations>
   </div>
+
 </template>
 
 <script>
-import vForVIfPrecedenceChange from "./components/v-for-v-if-precedence-change";
-import vBindOrderSensitive from "./components/v-bind-order-sensitive"
-import vElseIfKey from "./components/v-else-if-key";
-import TransitionGroupRoot from "./components/transition-group-root";
-import VForTemplateKey from "./components/v-for-template-key";
-
+import transformations from "@/views/transformations";
 export default {
   name: 'App',
   components: {
-    VForTemplateKey,
-    TransitionGroupRoot,
-    vForVIfPrecedenceChange,
-    vBindOrderSensitive,
-    vElseIfKey
+    transformations
   }
 }
 </script>
