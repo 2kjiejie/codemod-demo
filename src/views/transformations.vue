ed39fb1f276a1e1991f54807f09030f9d2c3c9af
2kjiejie <821208217@qq.com>
2021-06-25 11:58:27 +0800

    remove useless rules

--- src/views/transformations.vue
+++ src/views/transformations.vue
@@ -7,14 +7,6 @@
     <remove_set_and_delete></remove_set_and_delete>
     <rename_lifecycle></rename_lifecycle>
     <render_to_resolveComponent></render_to_resolveComponent>
-
-
-
-
-    <h1>scoped-slots-to-slots</h1>
-    <scoped-slots-to-slots :level="1" class="scopedSlot">Hello</scoped-slots-to-slots>
-    <v-model></v-model>
-    <vue-class-component></vue-class-component>
   </div>
 </template>
 
@@ -26,17 +18,9 @@ import remove_set_and_delete from "@/components/remove_set_and_delete";
 import rename_lifecycle from "@/components/rename_lifecycle";
 import render_to_resolveComponent from "@/components/render_to_resolveComponent";
 
-
-import ScopedSlotsToSlots from "../components/scoped-slots-to-slots";
-import VModel from "../components/v-model";
-import VueClassComponent from "../components/vue-class-component";
-
 export default {
   name: "transformations",
   components:{
-    ScopedSlotsToSlots,
-    VModel,
-    VueClassComponent,
     import_composition_api_from_vue,
     add_emit_declaration,
     new_directive_api,
