7e9e93e7df2ceda0e0e91a3551aae85c147fd43c
FireM <fire.mars.j@163.com>
2021-06-25 11:14:44 +0800

    router change to the left

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,59 @@
 <template>
-  <div>
+  <div class="outer nav-links">
     <p></p>
-    <router-link :to="{name:'home'}">Home</router-link>
-    ||
-    <router-link :to="{name:'transformations'}">transformations</router-link>
-    ||
-    <router-link :to="{name:'vue-transformation'}">vue-transformation</router-link>
-    <transition name="fade" mode="out-in">
+    <div class="sidebar links">
+      <router-link :to="{name:'home'}">Home</router-link>
+      <p></p>
+      <router-link :to="{name:'transformations'}">transformations</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'add_emit_declaration'}">add_emit_declaration</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'import_composition_api_from_vue'}">import_composition_api-from-vue</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'new_directive_api'}">new_directive_api</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'remove_set_and_delete'}">remove_set_and_delete</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'rename_lifecycle'}">rename_lifecycle</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'render_to_resolveComponent'}">render_to_resolveComponent</router-link>
+      <p></p>
+      <router-link :to="{name:'vue-transformation'}">vue-transformation</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'v-for-v-if-precedence-change'}">v-for-v-if-precedence-change</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'v-bind-order-sensitive'}">v-bind-order-sensitive</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'v-else-if-key'}">v-else-if-key</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'transition-group-root'}">transition-group-root</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'v-for-template-key'}">v-for-template-key</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'slot-scope-attribute'}">slot-scope-attribute</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'slot-attribute'}">slot-attribute</router-link>
+      <p></p>
+      &nbsp;
+      <router-link :to="{name:'slot-default'}">slot-default</router-link>
+      <p></p>
+    </div>
+    <div class="content">
       <router-view :key="$route.path"></router-view>
-    </transition>
+    </div>
   </div>
 
 </template>
@@ -37,12 +82,45 @@ export default {
 }
 
 .fade-enter-active,
-.fade-leave-active{
+.fade-leave-active {
   transition: opacity 0.3s;
 }
 
 .fade-enter,
-.fade-leave-to{
+.fade-leave-to {
   opacity: 0;
+}
+
+
+.sidebar {
+  width: 300px;
+  height: 100%;
+  display: table-cell;
+  position: fixed;
+  z-index: 1;
+  border-right: 1px solid grey;
+}
+
+.content {
+  margin-left: 350px;
+}
+
+.outer {
+  display: table;
+  width: 100%;
+}
+
+.nav-links{
+  display: flex;
+}
+
+.links{
+  padding-right: 20px;
+  list-style: none;
+}
+
+.links:hover{
+  /*text-decoration: underline;*/
+  color: blue;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,21 @@ import VueRouter from "vue-router";
 import transformations from "@/views/transformations";
 import vueTransformation from '@/views/vue-transformation'
 import hello from "@/views/hello";
+import add_emit_declaration from "@/components/add_emit_declaration";
+import import_composition_api_from_vue from "@/components/import_composition_api_from_vue";
+import new_directive_api from "@/components/new_directive_api";
+import remove_set_and_delete from "@/components/remove_set_and_delete";
+import rename_lifecycle from "@/components/rename_lifecycle";
+import render_to_resolveComponent from "@/components/render_to_resolveComponent";
+
+import vForVIfPrecedenceChange from "@/components/v-for-v-if-precedence-change";
+import vBindOrderSensitive from "@/components/v-bind-order-sensitive";
+import vElseIfKey from  "@/components/v-else-if-key"
+import transitionGroupRoot from "@/components/transition-group-root";
+import vForTemplateKey from "@/components/v-for-template-key"
+import slotScopeAttribute from "@/components/slot-scope-attribute-parent";
+import slotAttribute from '@/components/slot-attribute';
+import slotDefault from  '@/components/slot-default';
 
 Vue.use(VueRouter)
 const routes=[
@@ -19,11 +34,97 @@ const routes=[
         props:true
     },
     {
+        path:'/add_emit_declaration',
+        name:'add_emit_declaration',
+        component:add_emit_declaration,
+        props:true
+    },
+    {
+        path:'/import_composition_api_from_vue',
+        name:'import_composition_api_from_vue',
+        component:import_composition_api_from_vue,
+        props:true
+    },
+    {
+        path:'/new_directive_api',
+        name:'new_directive_api',
+        component:new_directive_api,
+        props:true
+    },
+    {
+        path:'/remove_set_and_delete',
+        name:'remove_set_and_delete',
+        component:remove_set_and_delete,
+        props:true
+    },
+    {
+        path:'/rename_lifecycle',
+        name:'rename_lifecycle',
+        component:rename_lifecycle,
+        props:true
+    },
+    {
+        path:'/render_to_resolveComponent',
+        name:'render_to_resolveComponent',
+        component:render_to_resolveComponent,
+        props:true
+    },
+    //scoped-slots-to-slots
+    {
         path:'/vue-transformation',
         name:'vue-transformation',
         component: vueTransformation,
         props:true
-    }
+    },
+    {
+        path:'/v-for-v-if-precedence-change',
+        name:'v-for-v-if-precedence-change',
+        component:vForVIfPrecedenceChange,
+        props:true
+    },
+    {
+        path:'/v-bind-order-sensitive',
+        name:'v-bind-order-sensitive',
+        component:vBindOrderSensitive,
+        props:true
+    },
+    {
+        path:'/v-else-if-key',
+        name:'v-else-if-key',
+        component:vElseIfKey,
+        props:true
+    },
+    {
+        path:'/transition-group-root',
+        name:'transition-group-root',
+        component:transitionGroupRoot,
+        props:true
+    },
+    {
+        path:'/v-for-template-key',
+        name:'v-for-template-key',
+        component:vForTemplateKey,
+        props:true
+    },
+    {
+        path:'/slot-scope-attribute',
+        name:'slot-scope-attribute',
+        component:slotScopeAttribute,
+        props:true
+    },
+    {
+        path:'/slot-attribute',
+        name:'slot-attribute',
+        component:slotAttribute,
+        props:true
+    },
+    {
+        path:'/slot-default',
+        name:'slot-default',
+        component:slotDefault,
+        props:true
+    },
+
 ]
 
 const router=new VueRouter({
